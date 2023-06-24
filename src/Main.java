41dfbd3b4fefff18045e1988eafbbb406abd236d
Deivid-Dimas <soporteneca@outlook.com>
2023-06-24 18:46:52 -0400

    mergeSort Algorithm tested

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,14 @@ public class Main {
         for ( int i = 0 ; i < arr.length ; i++){
             arr[i] = random.nextInt(100);
         }
-        System.out.println("Starting QuickSort!");
-        QuickSort.quickSort(arr);
+        System.out.println("Starting Sort!");
+        for ( int i = 0 ; i < arr.length ; i++){
+            System.out.print(" " + arr[i]);
+        }
+        //QuickSort.quickSort(arr);
+        MergeSort.mergeSort(arr);
+        System.out.println("\nMergeSorting");
+
         for ( int i = 0 ; i < arr.length ; i++){
             System.out.print(" " + arr[i]);
         }
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,6 +1,11 @@
+
 public class MergeSort {
 
     public static void mergeSort(int[] array){
+
+        if (array.length < 2){
+            return;
+        }
         int middle = array.length / 2;
         int[] leftArray = new int[middle];
         int[] rightArray = new int[array.length - middle];
@@ -8,11 +13,48 @@ public class MergeSort {
         for (int i = 0 ; i < middle ; i++){
             leftArray[i] = array[i];
         }
-        for (int i = 0 ; i < middle ; i++){
-            rightArray[i] = array[i];
+        for (int i = middle ; i < array.length ; i++){
+            rightArray[i - middle] = array[i];
         }
-    }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
-    public static void mergeSort(int[] array, int[] leftArray, int[] rightArray){
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(array,leftArray,rightArray);
+    }
+    private static void merge(int[] array, int[] leftArray, int[] rightArray){
+
+        int leftArraySize = leftArray.length;
+        int rightArraySize = rightArray.length;
+        int left_I = 0 , right_I = 0, mainIndex = 0;
+
+        while( left_I < leftArraySize && right_I < rightArraySize) {
+
+            if (leftArray[left_I] <= rightArray[right_I]) {
+
+                array[mainIndex] = leftArray[left_I];
+                left_I++;
+
+            } else {
+
+                array[mainIndex] = rightArray[right_I];
+                right_I++;
+
+            }
+            mainIndex++;
+
+
+
+        }
+        while (left_I < leftArraySize) {
+            array[mainIndex] = leftArray[left_I];
+            left_I++;
+            mainIndex++;
+        }
+
+        while (right_I < rightArraySize){
+            array[mainIndex] = rightArray[right_I];
+            right_I++;
+            mainIndex++;
+        }
 
     }
 
